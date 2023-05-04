58036bde2835aa72391c9964c9d772e348aeaed7
infoalan30 <128619868+infoalan30@users.noreply.github.com>
2023-05-04 15:50:16 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted text:', text);
+    // 这里可以将文本提交到服务器或进行其他操作
+  };
+
   return (
     <div className="container">
       <Head>
@@ -12,6 +21,13 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter text:
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
